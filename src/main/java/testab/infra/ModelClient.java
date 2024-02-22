42d39921e347cb377a1b40030aeca486e6fa0ce8
안성오 <sung5.ahn@kt.com>
2024-02-22 10:20:50 +0900

    env 별 model end point 반영

--- src/main/java/testab/infra/ModelClient.java
+++ src/main/java/testab/infra/ModelClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import testab.domain.Approved;
 
 
-@FeignClient(name = "models", url = "http://localhost:8088")
+@FeignClient(name = "models")
 public interface ModelClient {
   @GetMapping("/models/{id}")
   Approved callOtherService(@PathVariable("id") Long id);
